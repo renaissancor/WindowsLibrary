8364ccd52a4ab446872a4d37e96d6f78cd6d8edf
renaissancor <stephenjhpark99@gmail.com>
2025-09-23 18:49:00 +0900

    feat: no interlock spsc queue implemented

--- Library/Include/SPSCQueue.h
+++ Library/Include/SPSCQueue.h
@@ -7,14 +7,15 @@ template<typename T>
 class SPSCQueue {
 private:
 	constexpr static const size_t CACHE_LINE_SIZE = 64; 
-	alignas(CACHE_LINE_SIZE) volatile LONG _head{ 0 };
-	alignas(CACHE_LINE_SIZE) volatile LONG _tail{ 0 };
+	alignas(CACHE_LINE_SIZE) volatile size_t _head{ 0 };
+	alignas(CACHE_LINE_SIZE) volatile size_t _tail{ 0 };
 	T* _buffer;
 	size_t _capacity;
 
 public:
 	explicit SPSCQueue(size_t capacity) 
-		: _capacity(capacity) {
+		: _capacity(capacity) 
+	{
 		_buffer = new T[capacity]; 
 	}
 
@@ -31,34 +32,34 @@ public:
 
 template<typename T>
 bool SPSCQueue<T>::Push(const T& item) {
-	const LONG current_head = _head;
-	const LONG current_tail = _tail;
+	const size_t current_head = _head;
+	const size_t current_tail = _tail;
 	const size_t next_tail = (current_tail + 1) % _capacity;
 	if (next_tail == current_head) {
 		return false;
 	}
 	_buffer[current_tail] = item;
-	_InterlockedExchange((LONG volatile*)&_tail, next_tail);
+	_tail = next_tail;
 	return true;
 }
 
 template<typename T>
 bool SPSCQueue<T>::Pop(T& item) {
-	const LONG current_head = _head;
-	const LONG current_tail = _tail;
+	const size_t current_head = _head;
+	const size_t current_tail = _tail;
 	if (current_head == current_tail) {
 		return false;
 	}
 	item = _buffer[current_head];
 	const size_t next_head = (current_head + 1) % _capacity;
-	_InterlockedExchange((LONG volatile*)&_head, next_head);
+	_head = next_head; 
 	return true;
 }
 
 template<typename T>
 const T* SPSCQueue<T>::Front() {
-	const LONG current_head = _head;
-	const LONG current_tail = _tail; 
+	const size_t current_head = _head;
+	const size_t current_tail = _tail;
 	if (current_head == current_tail) {
 		return nullptr;
 	}
@@ -67,13 +68,13 @@ const T* SPSCQueue<T>::Front() {
 
 template<typename T>
 const T* SPSCQueue<T>::PopFront() {
-	const LONG current_head = _head;
-	const LONG current_tail = _tail;
+	const size_t current_head = _head;
+	const size_t current_tail = _tail;
 	if(current_head == current_tail) {
 		return nullptr;
 	}
 	const T* item = &_buffer[current_head];
 	const size_t next_head = (current_head + 1) % _capacity; 
-	_InterlockedExchange((LONG volatile*)&_head, next_head);
+	_head = next_head;
 	return item;
 }
