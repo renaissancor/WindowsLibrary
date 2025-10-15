f444bdab0ec5a8a75a20ac186d8b7e872b78b50e
renaissancor <stephenjhpark99@gmail.com>
2025-10-15 21:10:46 +0900

    feat: guard overflow cpp header segregated

--- Library/Include/GuardOverflow.h
+++ Library/Include/GuardOverflow.h
@@ -14,61 +14,23 @@ namespace GuardOverflow {
 		unordered_map<uintptr_t, AllocInfo> _info;
 		size_t _page_size; // PAGE SIZE  
 		DWORD _AllocationGranularity; 
+		CRITICAL_SECTION _cs; 
 
 	private:
-		Manager(): _page_size(4096), _AllocationGranularity(0) {
-			SYSTEM_INFO si; 
-			GetSystemInfo(&si); 
-			_AllocationGranularity = si.dwAllocationGranularity; 
-			_page_size = si.dwPageSize;
-		}
+		Manager();
+		~Manager();
 
-		void FreeAllMemory() noexcept {
-			for (auto& pair : _info) {
-				VirtualFree((LPVOID)pair.second.page_base, 0, MEM_RELEASE);
-			}
-			_info.clear();
-		}
-
-		~Manager() {
-			FreeAllMemory(); 
-		}
-
+		void FreeAllMemory() noexcept;
+		
 	public:
 		inline static Manager& GetInstance() noexcept {
 			static Manager instance; 
 			return instance; 
 		}
-	
-		LPVOID Alloc(size_t bytes_to_alloc) noexcept {
-			if (bytes_to_alloc == 0) return NULL;
-
-			const size_t PAGE = _page_size;
-			size_t page_count = (bytes_to_alloc + PAGE - 1) / PAGE + 1; // +1 for guard page
-			size_t total_bytes = page_count * PAGE;
-
-			uintptr_t ptr_page_base = (uintptr_t)
-				VirtualAlloc(NULL, total_bytes, MEM_COMMIT, PAGE_READWRITE);
-			if (!ptr_page_base) return NULL;
-
-			uintptr_t ptr_page_guard = ptr_page_base + (page_count - 1) * PAGE;
-			DWORD oldProtect = 0;
-			VirtualProtect((LPVOID)ptr_page_guard, PAGE, PAGE_NOACCESS, &oldProtect);
-
-			uintptr_t ptr_return = ptr_page_guard - bytes_to_alloc;
-			AllocInfo info{ ptr_page_base, total_bytes };
-			_info[ptr_return] = info;
-
-			return (LPVOID)ptr_return;
-		}
-
-		void Free(LPVOID ptr) noexcept {
-			auto it = _info.find((uintptr_t)ptr);
-			if (it != _info.end()) {
-				VirtualFree((LPVOID)it->second.page_base, 0, MEM_RELEASE);
-				_info.erase(it);
-			}
-		}
+		
+		LPVOID Alloc(size_t bytes_to_alloc) noexcept;
+		void Free(LPVOID ptr) noexcept;
+		
 	};
 
 	inline LPVOID Alloc(size_t bytes_to_alloc) noexcept {
--- Library/Sources/GuardOverflow.cpp
+++ Library/Sources/GuardOverflow.cpp
@@ -1,2 +1,65 @@
 #include "pch.h"
 #include "GuardOverflow.h" 
+
+// GuardOverflow.cpp 
+
+GuardOverflow::Manager::Manager() 
+	: _page_size(4096), _AllocationGranularity(0) {
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	_AllocationGranularity = si.dwAllocationGranularity;
+	_page_size = si.dwPageSize;
+	InitializeCriticalSection(&_cs);
+}
+
+GuardOverflow::Manager::~Manager() {
+	FreeAllMemory(); 
+	DeleteCriticalSection(&_cs);
+}	
+
+void GuardOverflow::Manager::FreeAllMemory() noexcept {
+	for (auto& pair : _info) {
+		VirtualFree((LPVOID)pair.second.page_base, 0, MEM_RELEASE);
+	}
+	_info.clear();
+}
+
+LPVOID GuardOverflow::Manager::Alloc(size_t bytes_to_alloc) noexcept {
+	if (bytes_to_alloc == 0) return NULL;
+	EnterCriticalSection(&_cs);
+	const size_t PAGE = _page_size;
+	size_t page_count = (bytes_to_alloc + PAGE - 1) / PAGE + 1; // +1 for guard page
+	size_t total_bytes = page_count * PAGE;
+	uintptr_t ptr_page_base = (uintptr_t)
+		VirtualAlloc(NULL, total_bytes, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (!ptr_page_base) {
+		LeaveCriticalSection(&_cs);
+		return NULL;
+	}
+	uintptr_t ptr_page_guard = ptr_page_base + (page_count - 1) * PAGE;
+	DWORD oldProtect = 0;
+	if (!VirtualProtect((LPVOID)ptr_page_guard, PAGE, PAGE_NOACCESS, &oldProtect)) {
+		VirtualFree((LPVOID)ptr_page_base, 0, MEM_RELEASE);
+		LeaveCriticalSection(&_cs);
+		return NULL;
+	}
+	uintptr_t ptr_return = ptr_page_guard - bytes_to_alloc;
+	AllocInfo info{ ptr_page_base, total_bytes };
+	_info[ptr_return] = info;
+	LeaveCriticalSection(&_cs);
+	return (LPVOID)ptr_return;
+}
+
+// Note: Returned pointer is not page base, it's placed before the guard page.
+// You must pass the same pointer to GuardOverflow::Free().
+
+void GuardOverflow::Manager::Free(LPVOID ptr) noexcept {
+	if (!ptr) return;
+	EnterCriticalSection(&_cs);
+	auto it = _info.find((uintptr_t)ptr);
+	if (it != _info.end()) {
+		VirtualFree((LPVOID)it->second.page_base, 0, MEM_RELEASE);
+		_info.erase(it);
+	}
+	LeaveCriticalSection(&_cs);
+}
